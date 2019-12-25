2c5ae7816b497a00ee6ddba5ac172199cdc3b0eb
Chris <chrisjarmstrong97@gmail.com>
2019-12-25 19:21:36 +1100

    Day 2 part 2 complete, brute force

--- src/Gravity.java
+++ src/Gravity.java
@@ -24,41 +24,68 @@ public class Gravity {
 		}
 		
 		reader.close();
-		System.out.println(system.calculateGravity());
+		System.out.println(system.calculateGravity(nums));
 	}
 	
-	public Integer calculateGravity() {
-		this.opcodes.put(1, 12);
-		this.opcodes.put(2, 2);
+	public Integer calculateGravity(String[] nums) {
 		Boolean valid = true;
 		Integer current = 0;
-		while(valid) {
-			Integer firstVal;
-			Integer secondVal;
-			Integer result;
-			switch (this.opcodes.get(current)) {
-				case 1 :
-					firstVal = this.opcodes.get(this.opcodes.get(current+1));
-					secondVal = this.opcodes.get(this.opcodes.get(current+2));
-					result = firstVal + secondVal;
-					this.opcodes.put(this.opcodes.get(current+3), result);
+		Boolean noResult = true;
+		Integer outerLoop = 0;
+		Integer innerLoop = 0;
+		
+		while(noResult) {
+			innerLoop = 0;
+			while(innerLoop < 100) {
+				valid = true;
+				this.opcodes.put(1, outerLoop);
+				this.opcodes.put(2, innerLoop);
+				current = 0;
+				while(valid) {
+					Integer firstVal;
+					Integer secondVal;
+					Integer result;
+					switch (this.opcodes.get(current)) {
+						case 1 :
+							firstVal = this.opcodes.get(this.opcodes.get(current+1));
+							secondVal = this.opcodes.get(this.opcodes.get(current+2));
+							result = firstVal + secondVal;
+							this.opcodes.put(this.opcodes.get(current+3), result);
+							break;
+						case 2 :
+							firstVal = this.opcodes.get(this.opcodes.get(current+1));
+							secondVal = this.opcodes.get(this.opcodes.get(current+2));
+							result = firstVal * secondVal;
+							this.opcodes.put(this.opcodes.get(current+3), result);
+							break;
+						case 99 :
+							System.out.println("Done");
+							valid = false;
+							break;
+						default :
+							System.out.println("You have made a dumb");
+							valid = false;
+					}
+					current = current + 4;
+				}
+				
+				if (this.opcodes.get(0) == 19690720) {
+					System.out.println("noun: " + outerLoop + " verb: " + innerLoop + " result = " + this.opcodes.get(0));
+					System.out.println("answer= " + (100*outerLoop+innerLoop));
+					noResult = false;
 					break;
-				case 2 :
-					firstVal = this.opcodes.get(this.opcodes.get(current+1));
-					secondVal = this.opcodes.get(this.opcodes.get(current+2));
-					result = firstVal * secondVal;
-					this.opcodes.put(this.opcodes.get(current+3), result);
-					break;
-				case 99 :
-					System.out.println("Done");
-					valid = false;
-					break;
-				default :
-					System.out.println("You have made a dumb");
-					valid = false;
+				}
+				
+				//reset codes
+				Integer position = 0;
+				for (String i : nums) {
+					this.opcodes.put(position, Integer.parseInt(i));
+					position++;
+				}
+				innerLoop++;
 			}
-			current = current + 4;
+			outerLoop++;
 		}
-		return this.opcodes.get(0);
+		return 0;
 	}
 }
